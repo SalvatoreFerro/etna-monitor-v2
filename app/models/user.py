0150773d491273eba6caee7874225169a2267378
SalvatoreFerro <71822226+SalvatoreFerro@users.noreply.github.com>
2025-10-25 15:00:29 +0200

    Ensure OAuth users have empty password hash

--- app/models/user.py
+++ app/models/user.py
@@ -5,7 +5,7 @@ class User(db.Model):
     
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(120), unique=True, nullable=False)
-    password_hash = db.Column(db.String(128), nullable=True)
+    password_hash = db.Column(db.String(128), nullable=True, default="")
     premium = db.Column(db.Boolean, default=False, nullable=False)
     is_admin = db.Column(db.Boolean, default=False, nullable=False)
     chat_id = db.Column(db.String(50), nullable=True)
