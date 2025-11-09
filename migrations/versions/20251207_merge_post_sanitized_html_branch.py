aa2659aecd630038c690a9de38826269c3402735
SalvatoreFerro <71822226+SalvatoreFerro@users.noreply.github.com>
2025-11-09 15:18:26 -0800

    Add merge migration to unify alembic heads

---
+++ migrations/versions/20251207_merge_post_sanitized_html_branch.py
@@ -0,0 +1,22 @@
+"""Merge post sanitized HTML branch with community moderation chain."""
+
+from __future__ import annotations
+
+
+revision = "20251207_merge_post_sanitized_html_branch"
+down_revision = (
+    "202502140001_add_post_sanitized_html",
+    "20251206_account_community_moderation",
+)
+branch_labels = None
+depends_on = None
+
+
+def upgrade() -> None:
+    """No-op merge revision."""
+    pass
+
+
+def downgrade() -> None:
+    """No-op merge revision."""
+    pass
