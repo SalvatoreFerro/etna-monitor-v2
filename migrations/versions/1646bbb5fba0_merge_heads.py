35bf8235633183e8e83a7154bda306f441cb6aef
SalvatoreFerro <71822226+SalvatoreFerro@users.noreply.github.com>
2026-01-04 17:48:20 +0100

    Add merge migration for alembic heads

---
+++ migrations/versions/1646bbb5fba0_merge_heads.py
@@ -0,0 +1,17 @@
+"""merge heads"""
+
+revision = "1646bbb5fba0"
+down_revision = ('20251215_add_cron_runs', 'c51695aaef1e')
+branch_labels = None
+depends_on = None
+
+from alembic import op
+import sqlalchemy as sa
+
+
+def upgrade() -> None:
+    pass
+
+
+def downgrade() -> None:
+    pass
